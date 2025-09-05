ddde4cf2160a702d2fd37f51c700eafb29b8e951
cerenece <132522061+cerenece@users.noreply.github.com>
2025-09-05 16:18:05 +0300

    Railway için chromium ve chromedriver ayarlandı

--- localch/localch/spiders/localch_spider.py
+++ localch/localch/spiders/localch_spider.py
@@ -21,11 +21,12 @@ class LocalchSeleniumSpider(scrapy.Spider):
         chrome_options = Options()
         chrome_options.add_argument("--headless=new")
         chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-dev-shm-usage")  # /dev/shm problemi için
         chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--disable-software-rasterizer")
+        chrome_options.add_argument("--disable-extensions")
         chrome_options.add_argument("--window-size=1920,1080")
-        chrome_options.add_argument("--disable-features=NetworkService")
-        chrome_options.add_argument("--ignore-certificate-errors")
+        chrome_options.add_argument("--remote-debugging-port=9222")
         chrome_options.binary_location = os.environ.get("CHROMIUM_PATH", "/usr/bin/chromium")
 
         self.driver = webdriver.Chrome(
